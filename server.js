8a8dc9232c420918272f18df6e1d97f176a3cddc
Ramesh Kumar <ramesh969399@gmail.com>
2026-05-06 23:12:42 +0530

    fix express options wildcard crash

--- server.js
+++ server.js
@@ -112,7 +112,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Handle preflight requests
-app.options("*", cors(corsOptions));
+// app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 app.use(express.json());
 
